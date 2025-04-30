bf9df48795343a9eeef90ae5bf11d4e51932a2fa
Fgodinez5060 <fgodinez@my.gcu.edu>
2025-04-30 01:54:58 -0700

    Add 5-minute break option to study timer

--- study_timer.py
+++ study_timer.py
@@ -9,10 +9,21 @@ try:
     print(f"Starting a {minutes}-minute study session...")
 
     for i in range(minutes):
-        print(f"minute {i+1} of {minutes}")
+        print(f"Minute {i+1} of {minutes}")
         time.sleep(1)
 
-    print("Study session over! Time for a break")
+    response = input("Do you want to take a 5-minute break? (yes/no) ").lower()
+    if response == "yes":
+        for i in range(5):
+            print(f"Minute {i+1} of 5")
+            time.sleep(1)
+        print("Break over. Time to get back to work!")
+
+    elif response == "no":
+        print("Okay, session complete. Nice work!")
+
+    else:
+        print("Improper response.")
 
 except ValueError:
     print("Please enter a valid number.")
